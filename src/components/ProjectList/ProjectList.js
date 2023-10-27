3afb120b6b8dc9978a838bd151bbc92ca767f6d3
Phalco1 <elphalco1@gmail.com>
2023-10-27 12:41:26 -0500

    got some projects... kinda

---
+++ src/components/ProjectList/ProjectList.js
@@ -0,0 +1,25 @@
+// import  phalcohub_photo  from "../assets/images/phalcohub_photo.png";
+
+export const ProjectList = [
+    {
+        name: "Phalcohub",
+        image: "phalcohub_photo.png",
+        skills: "ReactJS, Node.js, HTML, CSS"
+    },
+   
+    {
+        name: "Tic-Tac-Toe",
+        image: "phalcohub_photo.png",
+        skills: "ReactJS, Node.js, HTML, CSS"
+    },
+    {
+        name: "4 Corners: Malik's Adventures",
+        image: "phalcohub_photo.png",
+        skills: "ReactJS, Node.js, HTML, CSS"
+    },
+    {
+        name: "4 Corners: Arena",
+        image: "phalcohub_photo.png",
+        skills: "ReactJS, Node.js, HTML, CSS"
+    }
+];
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,34 +1,43 @@
+import React, { useEffect, useState }from 'react';
+import ProjectItem from '../ProjectItem/index.js';
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
-import { useEffect, useState } from 'react';
 import Loader from 'react-loaders';
+import { ProjectList } from '../ProjectList/ProjectList';
 
-const Project = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
-    
+
+function Project() {
+    const [letterClass, setLetterClass] = useState('text-animate');
+
 
     useEffect(() => {
         setTimeout(() => {
-           setLetterClass('text-animate-hover');
-       }, 3000);
-   }, [])
+            setLetterClass('text-animate-hover');
+        }, 3000);
+    }, []);
+
+
 
     return (
-    <>
-        <div className='container contact-page'>
-            <div className='text-zone'>
-                <h1>
-                    <AnimatedLetters 
-                    letterClass={letterClass}
-                    strArray={["P", "o", "r", "t", "f", "o", "l", "i", "o"]}
-                    idx={15}
-                    />
-                </h1>
+        <>
+            <div className='container contact-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={["P", "o", "r", "t", "f", "o", "l", "i", "o"]}
+                            idx={15} />
+                    </h1>
+                <div className='projectItem'>
+                    {ProjectList.map((project, idx) => {
+                        return <ProjectItem id={idx} name={project.name} image={project.image} />;
+                    })}
+                </div>
+                </div>
+                <Loader type='pacman' />
             </div>
-            <Loader type='pacman' />
-        </div>
-    </>
-    )
+        </>
+    );
 }
 
 
